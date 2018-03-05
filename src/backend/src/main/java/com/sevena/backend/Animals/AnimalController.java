a3f29298ee543821c7fa219f88e2434c9190f3f4
bahorn <hornb@uni.coventry.ac.uk>
2018-03-05 14:30:58 +0000

    Now returns Animal JSON

--- src/backend/src/main/java/com/sevena/backend/Animals/AnimalController.java
+++ src/backend/src/main/java/com/sevena/backend/Animals/AnimalController.java
@@ -4,18 +4,24 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sevena.backend.general.*;
 
 @RestController
 public class AnimalController {
 
     // Get details on an animal by it's ID.
     @RequestMapping("/animals/{animalid}")
-    public ResponseEntity<String> animal(@PathVariable("animalid") String animalId) {
+    public ResponseEntity<Animal> animal(@PathVariable("animalid") String animalId) {
         HttpHeaders responseHeaders = new HttpHeaders();
         System.out.println(animalId);
-        return new ResponseEntity<String>("HELLO WORLD", responseHeaders, HttpStatus.CREATED);
+        Location location = new Location();
+        Properties properties = new Properties();
+        Animal cool = new Animal(animalId, "A", "A", "A", "A", location, properties, "A");
+        return new ResponseEntity<Animal>(cool, responseHeaders, HttpStatus.CREATED);
     }
 
     // Search
--- src/backend/src/main/java/com/sevena/backend/General/Properties.java
+++ src/backend/src/main/java/com/sevena/backend/General/Properties.java
@@ -1,5 +1,13 @@
 package com.sevena.backend.general;
 
 public class Properties {
+    private final int id;
+    
+    public Properties() {
+        this.id = 1;
+    }
 
+    public int getId() {
+        return id;
+    }
 }
