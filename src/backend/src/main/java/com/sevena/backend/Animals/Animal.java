2a8ccf3fa747d2d7a92126486c66c2debc31c6c9
bahorn <hornb@uni.coventry.ac.uk>
2018-03-05 14:08:19 +0000

    updated backend.

--- src/backend/src/main/java/com/sevena/backend/Animals/Animal.java
+++ src/backend/src/main/java/com/sevena/backend/Animals/Animal.java
@@ -1,22 +1,25 @@
 package com.sevena.backend.animals;
 
+// MongoDB requires this.
+import org.springframework.data.annotation.Id;
+
 // Our standard imports
 import com.sevena.backend.general.Location;
 import com.sevena.backend.general.Properties;
 
 public class Animal {
-    private final long id;
+    @Id private final String id;
     private final String name;
     private final String type;
     private final String desc;
     private final String status;
     private final Location location;
     private final Properties properties;
-    private final long shelterId;
+    private final String shelterId;
 
-    public Animal(long id, String name, String type, String desc,
+    public Animal(String id, String name, String type, String desc,
             String status, Location location, Properties properties,
-            long shelterId) {
+            String shelterId) {
         this.id = id;
         this.name = name;
         this.type = type;
@@ -26,8 +29,33 @@ public class Animal {
         this.properties = properties;
         this.shelterId = shelterId;
     }
-
-    public long getId() {
+    
+    // Damn it Java.... Have to define all the getters....
+    public String getId() {
         return id;
+    }
+
+    public String getShelter() {
+        return shelterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Location location() {
+        return location;
+    }
+    
+    public Properties getProperties() {
+        return properties;
     }
 }
--- src/backend/src/main/java/com/sevena/backend/Animals/AnimalController.java
+++ src/backend/src/main/java/com/sevena/backend/Animals/AnimalController.java
@@ -2,14 +2,26 @@ package com.sevena.backend.animals;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnimalController {
 
-    @RequestMapping("/animal")
-    public String animal() {
-        return "A";//new Animal();
+    // Get details on an animal by it's ID.
+    @RequestMapping("/animals/{animalid}")
+    public ResponseEntity<String> animal(@PathVariable("animalid") String animalId) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        System.out.println(animalId);
+        return new ResponseEntity<String>("HELLO WORLD", responseHeaders, HttpStatus.CREATED);
+    }
+
+    // Search
+    @RequestMapping(value="/animals", method = RequestMethod.POST)
+    public ResponseEntity<String> searchAnimals() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
     }
 }
